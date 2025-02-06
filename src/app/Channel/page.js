229ef48a6dd922206cdc84d7f0eca85cd9f635a0
lucasns06 <lucasnascimento.carreira@gmail.com>
2025-02-06 16:49:16 -0300

    🚑️erro de build por causa da falta de fallback

--- src/app/Channel/page.js
+++ src/app/Channel/page.js
@@ -1,68 +1,10 @@
-'use client'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import './Channel.css'
-import { faArrowAltCircleDown, faBell } from '@fortawesome/free-solid-svg-icons';
-import { useSearchParams } from 'next/navigation';
-import { useVideo } from '../VideoContext';
-import Link from 'next/link';
-import { useSidebar } from '../SidebarContext';
-import '../Main/Main.css'
-import Sidebar from '../Sidebar/Sidebar';
-import '../media-queries.css'
-const Channel = () => {
-    const searchParams = useSearchParams();
-    const channelId = parseInt(searchParams.get('id'));
-    const { subs, videos } = useVideo();
-    const channelSelected = subs.find((item) => item.id === channelId);
-    const { sidebarIsOpen } = useSidebar();
+import { Suspense } from "react"; 
+import Channel from "./Channel";
 
-    return (
-        <>
-            <Sidebar />
-            <div className={`channel__container ${sidebarIsOpen ? "channel__resizer" : ""}`}>
-                <div className="banner">
-                    <img className="banner__background" src="/banner.jpg" alt="banner" />
-                </div>
-                <div className="channel_info flex items-center gap-4 pt-4">
-                    <img className="rounded-full" src={channelSelected.url} alt="logo" width={160} height={160} />
-                    <div className="channel__info__text flex flex-col gap-2">
-                        <h1 className='text-4xl font-bold'>{channelSelected.label}</h1>
-                        <p className='font-bold'>@{channelSelected.label} <span className='font-normal text-gray-300'>100 mil inscritos</span></p>
-                        <p className='text-gray-300'>Descrição do canal, ele abre um modal ;/</p>
-                        <div className="buttons__container__channel flex items-center gap-2">
-                            <button className="rounded-3xl py-2 pr-2">
-                                <FontAwesomeIcon
-                                    icon={faBell}
-                                    width={32}
-                                    height={32}
-                                    color="white" />
-                                Compartilhar
-                            </button>
-                            <button className="rounded-3xl py-2 pr-2">
-                                <FontAwesomeIcon
-                                    icon={faArrowAltCircleDown}
-                                    width={32}
-                                    height={32}
-                                    color="white" />
-                                Download
-                            </button>
-                        </div>
-                    </div>
-                </div>
-                <div className={`videos__container__channel p-6 grid gap-4`}>
-                    {videos.map((item) => (
-                        <Link href={`/Watch?id=${item.id}`} className="video__card" key={item.id} onClick={() => setLoading(true)}>
-                            <img src={item.img} className="thumb rounded-2xl" alt='thumb' />
-                            <div className="container__texto">
-                                <h1>{item.titulo}</h1>
-                                <p>{item.canal}</p>
-                                <p>{item.visu} mil visualizações</p>
-                            </div>
-                        </Link>
-                    ))}
-                </div>
-            </div>
-        </>
-    )
+export default function ChannelPage() {
+  return (
+    <Suspense fallback={<div>Carregando...</div>}>
+      <Channel />
+    </Suspense>
+  );
 }
-export default Channel;
